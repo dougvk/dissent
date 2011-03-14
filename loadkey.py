3180d92ff0e7079cab556523a33b1f28a61ecf75
Andrew Wiedemann <abw24@swan.zoo.cs.yale.edu>
2011-03-14 16:14:57 -0400

    cleaned up a bit

--- loadkey.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-from anon_crypto import AnonCrypto
-import M2Crypto.RSA
-
-loadedKey = M2Crypto.RSA.load_key('key')
-loadedKey.save_pub_key('keypub2')
--- makekey.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-from anon_crypto import AnonCrypto
-import M2Crypto.RSA
-
-print "starting crypto test"
-newKey = AnonCrypto.random_key(512)
-newKey.save_key('key', None)
-newKey.save_pub_key('keypub')
