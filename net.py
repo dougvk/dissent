4a3a3297df2a6f9937f22c187faa24ebc8976771
Andrew Wiedemann <abw24@swan.zoo.cs.yale.edu>
2011-03-14 15:49:49 -0400

    can create pub/priv keys

---
+++ net.py
@@ -0,0 +1,63 @@
+import os.path
+from anon_crypto import AnonCrypto
+import M2Crypto.RSA
+
+class Net:
+	
+	def __init__(self):
+		self.privKey = ''
+		self.pubKey = ''
+		print "net object constructed"
+	
+	
+	# load / create pub and priv keys in the config folder	
+	def establish_keys(self):
+		
+		if not os.path.exists('config'):
+			os.mkdir('config')
+				
+		try:
+			# load existing keys
+			privKey = M2Crypto.RSA.load_key('config/priv')
+			pubKey = M2Crypto.RSA.load_pub_key('config/pub')
+			
+		except:
+			
+			# generate new keys
+			print "key don't exist/valid"
+			newKey = AnonCrypto.random_key(512)
+			newKey.save_key('config/priv', None)
+			newKey.save_pub_key('config/pub')
+			
+			# delete existing configuration information
+			
+			# load keys
+			privKey = M2Crypto.RSA.load_key('config/priv')
+			pubKey = M2Crypto.RSA.load_pub_key('config/pub')
+
+
+	# invite a new node to join
+	def invite_node(self, nIP, nPort, nPub):
+			
+		
+			
+
+	# open a socket with all existing peers 
+	def open_sockets(self):
+		
+			print "opening sockets"
+		
+		
+
+			
+
+#def main():
+	
+#	newNet = Net()
+#	newNet.establish_keys()
+	
+	
+
+#if __name__ == '__main__':
+#	main()
+	
